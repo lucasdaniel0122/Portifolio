22252c2ba5b7632d0a0870cf31bb7d65ae696b34
𝘭𝘶𝘤𝘢𝘴 𝘥𝘢𝘯𝘪𝘦𝘭 <lucas.prado12@fatec.sp.gov.br>
2025-05-19 15:40:53 -0300

    adicionando funcionalidades a sessão de submenu "projetos"

--- menu.js
+++ menu.js
@@ -10,6 +10,27 @@ menu.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
+//Fechar o menu e rolar suavemente ao clicar em links do menu mobile//
+
+const linksMenuMobile = menu.querySelectorAll('nav ul li a')
+
+linksMenuMobile.forEach(link=> {
+    link.addEventListener('click', (event) => {
+        event.preventDefault() //impede o comportamento padrão do link//
+
+        //Fecha o menu//
+        menu.classList.remove('abrir-menu')
+
+        //Obtém o destino do link (hash)//
+        const href = link.getAttribute('href')
+
+        if(targetElement) {
+            //Rola suavemente até a sessão//
+            targetElement.scrollIntoView({ behavior: 'smooth' })
+        }
+    })
+})
+
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
